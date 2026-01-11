12dc18276dc6101958bd322305bda1f6d54f5fb8
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-11 13:35:16 +0530

    add user route and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,26 @@
-import dotenv from "dotenv";
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import { connectDB } from "./lib/db.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: " http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 app.use(clerkMiddleware());
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   connectDB();
